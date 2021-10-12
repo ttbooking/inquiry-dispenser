84eb4acef7106604177679e23c3e0fe237b18786
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-10-12 10:24:12 +0200

    Dispense job: timeout and WithoutOverlapping middleware removed

--- src/Jobs/Dispense.php
+++ src/Jobs/Dispense.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use TTBooking\InquiryDispenser\Exceptions\DispenseException;
 use TTBooking\InquiryDispenser\Subjects\IOMatch;
@@ -15,13 +14,6 @@ use TTBooking\InquiryDispenser\Subjects\IOMatch;
 class Dispense implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
-    /**
-     * The number of seconds the job can run before timing out.
-     *
-     * @var int
-     */
-    public $timeout = 120;
 
     /**
      * Execute the job.
@@ -35,15 +27,5 @@ class Dispense implements ShouldQueue, ShouldBeUnique
         } catch (\Throwable $e) {
             throw new DispenseException('Dispense operation failed.', $e->getCode(), $e);
         }
-    }
-
-    /**
-     * Get the middleware the job should pass through.
-     *
-     * @return array
-     */
-    public function middleware()
-    {
-        return [(new WithoutOverlapping)->dontRelease()->expireAfter(180)];
     }
 }
